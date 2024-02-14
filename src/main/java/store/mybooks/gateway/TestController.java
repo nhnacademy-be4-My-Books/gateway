27704e8c8c1ae5596b30ec17bc5c2db1a6547340
minsu <zmfjtnl1@naver.com>
2024-02-14 23:41:23 +0900

    test: jenkins test

---
+++ src/main/java/store/mybooks/gateway/TestController.java
@@ -0,0 +1,12 @@
+package store.mybooks.gateway;/**
+ *packageName    : store.mybooks.gateway
+ * fileName       : TestController
+ * author         : minsu11
+ * date           : 2/14/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2/14/24        minsu11       최초 생성
+ */public class TestController {
+}
