1971e35ca25c2533fec1d6e3e0bb090ad30dd864
masiljangajji <hjk172262@gmail.com>
2024-03-02 19:16:27 +0900

    feat: ErrorMessage Enum 추가

---
+++ src/main/java/store/mybooks/gateway/error/ErrorMessage.java
@@ -0,0 +1,28 @@
+package store.mybooks.gateway.error;
+
+import lombok.Getter;
+
+/**
+ * packageName    : store.mybooks.gateway.error<br>
+ * fileName       : ErrorMessage<br>
+ * author         : masiljangajji<br>
+ * date           : 3/2/24<br>
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 3/2/24        masiljangajji       최초 생성
+ */
+@Getter
+public enum ErrorMessage {
+    TOKEN_EXPIRED("Token has expired"),
+    INVALID_TOKEN("Invalid token"),
+    INVALID_ACCESS("Access forbidden");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+}
