cf96f51501328f0899e2ac639bf07b4daefd6601
masiljangajji <hjk172262@gmail.com>
2024-03-04 17:09:53 +0900

    feat: Jwt 비밀키 Key Manager를 통해 관리하도록

---
+++ src/main/java/store/mybooks/gateway/config/KeyMangerException.java
@@ -0,0 +1,18 @@
+package store.mybooks.gateway.config;
+
+/**
+ * packageName    : store.mybooks.front.config
+ * fileName       : KeyMangerException
+ * author         : Fiat_lux
+ * date           : 3/03/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 3/03/24        Fiat_lux       최초 생성
+ */
+public class KeyMangerException extends RuntimeException {
+    public KeyMangerException(String message) {
+        super(message);
+    }
+}
